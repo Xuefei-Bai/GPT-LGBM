7ac52f645a590aaa3c5ad6cda5d72e6a2f1856cb
Xuefei-Bai <77725552+Xuefei-Bai@users.noreply.github.com>
2025-10-02 05:06:24 +0800

    Update model_classifiers.py

--- model_classifiers.py
+++ model_classifiers.py
@@ -9,12 +9,9 @@ from sklearn.ensemble import RandomForestClassifier
 
 def RF(train_X,
        train_y,
-       val_X,
-       val_y,
        model_arg_dict=dict(),
        fit_arg_dict=dict()):
-    model = RandomForestClassifier(random_state=random_state_num,
-                                   **model_arg_dict)
+    model = RandomForestClassifier(random_state=random_state_num, **model_arg_dict)
     _ = model.fit(train_X, train_y, **fit_arg_dict)
     return model
 
@@ -33,3 +30,4 @@ def LGBM(train_X,
     _ = model.fit(train_X, train_y, **fit_arg_dict)
 
     return model
+
